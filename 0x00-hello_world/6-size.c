cbdae6188bbcff093c8db9367c30fd6abf775e61
dkelvin89 <dkelvin89@outlook.com>
2022-10-13 14:42:48 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Paints the size of var types
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of char: %d bytes(s)\n", sizeof(a));
+	printf("Size of an int: %d bytes(s)\n", sizeof(b));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(c));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(d));
+	printf("Size of a float: %d bytes(s)\n", sizeof(e));
+	return (0);
+}
