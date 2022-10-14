4879f0b11afa9edd81f3ef8d490c9ad3fea61459
dkelvin89 <dkelvin89@outlook.com>
2022-10-13 17:40:54 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main - Prints a text
+ *
+ * Return: should be 1 (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dore Korpar, 2015-10-19\n", 59);
+	return (1);
+}
