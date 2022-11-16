fcb8ae10edf9b0c73d1bf93b4393470f65dd1fd0
dkelvin89 <dkelvin89@outlook.com>
2022-11-16 01:56:09 -0800

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name.
+ * @name: Pointer
+ * @f: Pointer to function
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
